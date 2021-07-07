16543471ccbb8d790d3c4cc0510dc5b7177f1caf
Drew Silcock <drew.silcock@stfc.ac.uk>
2021-07-07 16:21:11 +0100

    Add binary files, symlinked to ~/bin/{script name}.

--- install.sh
+++ install.sh
@@ -21,6 +21,13 @@ create_symlink tmux/.tmux.conf .tmux.conf
 create_symlink .vimrc
 rm -rf ~/.vim && create_symlink vim .vim
 create_symlink .zshrc
+
+mkdir -p ~/bin
+for file_path in $(\ls ./bin/*); do
+    file_name=$(basename "$file_path")
+    create_symlink "bin/$file_name"
+done
+
 test -e .zsh_machine && create_symlink .zsh_machine
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
